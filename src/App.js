5c1835d27292dbb9e32e9c8c936adf0b03ac54d0
Niklas Söderberg <nma.prob@gmail.com>
2021-01-14 23:08:09 +0100

    create loginpage with browserrouter

--- src/App.js
+++ src/App.js
@@ -1,63 +1,30 @@
 import React, { useState } from "react"
+import { Switch, Route } from "react-router-dom"
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-
-  const [formData, setFormData] = useState({
-    email: "Niklas.Soderberg@yh.nackademin.se",
-    password: "javascriptoramverk"
-  })
-
-  function handleOnChange(e) {
-    setFormData({...formData, [e.target.name]: e.target.value})
-    // console.log({...formData, [e.target.name]: e.target.value})
-  }
-
-  function showPassword() {
-    let x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
-
-  function handleOnSubmit(e) {
-    e.preventDefault()
-    const url ="https://frebi.willandskill.eu/api-token-auth/"
-    const payload = {
-      email: formData.email,
-      password: formData.password
-    }
+  
+  function getSuperSecretLogInToken() {
+    const token = localStorage.getItem("superSecretLogInToken")
+    const url = "https://frebi.willandskill.eu/api/v1/me/"
     fetch(url, {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: {
-        "Content-Type": "application/json"
+      method: "GET",
+      header: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
       }
     })
-    .then(res => res.json())
-    .then(data => console.log(data))
-
   }
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <h1 className="ml-2">Welcome!</h1>
-          <form onSubmit={handleOnSubmit}>
-            <label className="col-md-2">Email:</label>
-            <input name="email" onChange={handleOnChange} value={formData.email} className="col-md-5" />
-            <p className="col-md-5" />
-            <label className="col-md-2">Password:</label>
-            <input id="password" name="password" onChange={handleOnChange} value={formData.password} className="col-md-5" type="password" />
-            <p className="col-md-5" />
-            <input type="checkbox" onClick={showPassword}/>
-            <p>Show Password</p>
-            <button className="col-md-2 btn btn-primary" type="submit">
-              Log In
-            </button>
-          </form>
+          <Switch>
+            <Route path="/login" >
+              <LoginPage/>
+            </Route>
+          </Switch>
         </div>
       </div>
     </div>
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+export default function LoginPage() {
+
+    const [formData, setFormData] = useState({
+        email: "Niklas.Soderberg@yh.nackademin.se",
+        password: "javascriptoramverk"
+    })
+
+    function handleOnChange(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+        // console.log({...formData, [e.target.name]: e.target.value})
+    }
+
+    function handleOnSubmit(e) {
+        e.preventDefault()
+        const url = "https://frebi.willandskill.eu/api-token-auth/"
+        const payload = {
+            email: formData.email,
+            password: formData.password
+        }
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                localStorage.setItem("superSecretLogInToken", data.token)
+            })
+    }
+
+    function showPassword() {
+        let x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+
+    return (
+        <div>
+            <h1 className="ml-2">Welcome!</h1>
+            <form onSubmit={handleOnSubmit}>
+                <label className="col-md-2">Email:</label>
+                <input name="email" onChange={handleOnChange} value={formData.email} className="col-md-5" />
+                <p className="col-md-5" />
+                <label className="col-md-2">Password:</label>
+                <input id="password" name="password" onChange={handleOnChange} value={formData.password} className="col-md-5" type="password" />
+                <p className="col-md-5" />
+                <input type="checkbox" onClick={showPassword} />
+                <p>Show Password</p>
+                <button className="col-md-2 btn btn-primary" type="submit">
+                    Log In
+                </button>
+            </form>
+        </div>
+    )
+}
