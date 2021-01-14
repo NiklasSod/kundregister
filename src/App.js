908b5930d3a0548630ad011d32e991fd5c75b9db
Niklas Söderberg <nma.prob@gmail.com>
2021-01-14 20:24:44 +0100

    loginpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import react from "react"
+import {  } from "react-router-dom"
 
 function App() {
   return (
-    <div>
-
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="ml-2">Welcome!</h1>
+          <form>
+            <label className="col-md-2">Email:</label>
+            <input className="col-md-3" />
+            <label className="col-md-2">Password:</label>
+            <input className="col-md-3" />
+            <br /> <br />
+            <button className="col-md-2 btn btn-primary" type="submit">Log In</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
